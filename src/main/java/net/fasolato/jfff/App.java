6f6961230986c37631827603844820e9f722fbc7
ut0729 <fasolato@omicronconsulting.it>
2017-11-03 17:13:04 +0100

    Empty Maven project

---
+++ src/main/java/net/fasolato/jfff/App.java
@@ -0,0 +1,13 @@
+package net.fasolato.jfff;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
